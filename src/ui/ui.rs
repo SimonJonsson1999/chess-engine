2280f411c4624027b7d8a70cc87605087a0e6d81
Simon <Simon.Jonsson@molnlycke.com>
2026-07-31 11:18:38 +0200

    resizeable window

--- src/ui/ui.rs
+++ src/ui/ui.rs
@@ -1,62 +1,72 @@
 use sdl3::pixels::Color;
-use sdl3::event::Event;
+use sdl3::event::{Event, WindowEvent};
 use sdl3::keyboard::Keycode;
 use std::time::Duration;
 use sdl3::rect::Rect;
-use sdl3::video::Window;
+use sdl3::video::{Window};
 use sdl3::render::Canvas;
+
+use std::cmp::min;
 use crate::engine::Engine;
 use crate::move_gen::BOARDWIDTH;
-
-const WIDTH: u32 = 800;
-const HEIGHT: u32 = 600;
-
-const SIDEBAR_WIDTH: u32 = 300;
-const STATUS_HEIGHT: u32 = 80;
-
-
-
-
 pub struct ChessUI {
     engine: Engine,
-    board_rect: Rect,
-    board_size: u32,
+    // window_size.0 is width, window_size.1 is height
+    window_size: (u32, u32),
 }
 
 impl ChessUI {
     pub fn new() -> Self {
-        let board_size: u32 = (WIDTH - SIDEBAR_WIDTH).min(HEIGHT - STATUS_HEIGHT);
         ChessUI{
             engine: Engine::new(),
-            board_rect: ChessUI::get_board_rect(board_size),
-            board_size
+            window_size: (800, 600)
         }
     }
 
     pub fn run(&mut self) {
         
-        let sdl_context = sdl3::init().unwrap();
-        let video_subsystem = sdl_context.video().unwrap();
-
-        let window = video_subsystem.window("Chess Engine UI", WIDTH, HEIGHT)
+        let sdl = sdl3::init().expect("Failed to initilize SDL3");
+        
+        let mut canvas = {
+            let video = sdl.video().expect("Failed to get display");
+            let window = video
+            .window("Chess Engine UI", self.window_size.0, self.window_size.1)
             .position_centered()
+            .resizable()
             .build()
-            .unwrap();
+            .expect("Failed to crate window");
 
-        let mut canvas = window.into_canvas();
+            window
+            .into_canvas()
 
-        let mut event_pump = sdl_context.event_pump().unwrap();
+        };
+        
+
+        let mut events = sdl.event_pump().expect("Failed to get event loop");
         'running: loop {
-            for event in event_pump.poll_iter() {
+            for event in events.poll_iter() {
                 match event {
-                    Event::Quit {..} |
-                    Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                        break 'running
-                    },
+                    Event::Quit { .. }
+                    | Event::KeyDown {
+                        keycode: Some(Keycode::Escape),
+                        ..
+                    } => {
+                        break 'running;
+                    }
+
+                    Event::Window {
+                        win_event: WindowEvent::Resized(width, height),
+                        ..
+                    } => {
+                        println!("Window resized to {}x{}", width, height);
+                        // Recalculate layout here
+                        self.window_size = (width as u32, height as u32);
+                    }
+
                     _ => {}
                 }
             }
-             canvas.set_draw_color(Color::RGB(30, 30, 30));
+            canvas.set_draw_color(Color::RGB(30, 30, 30));
             canvas.clear();
 
             // Draw everything
@@ -67,22 +77,23 @@ impl ChessUI {
         }
     }
 
-    pub fn get_board_rect(board_size: u32) -> Rect {
+    pub fn get_board_rect(&self) -> Rect {
         Rect::new(
             0,
             0,
-            board_size,
-            board_size,
+            self.window_size.0,
+            self.window_size.1,
         )
     } 
 
     pub fn draw_board(&mut self, canvas: &mut Canvas<Window>) {
-        let tile_size = self.board_size / 8;
-
+        let min_side = min(self.window_size.0, self.window_size.1);
+        let tile_size = min_side / 8;
+        let board_rect = self.get_board_rect();
         for rank in 0..BOARDWIDTH {
             for file in 0..BOARDWIDTH {
-                let x = self.board_rect.x() + file as i32 * tile_size as i32;
-                let y = self.board_rect.y() + rank as i32 * tile_size as i32;
+                let x = board_rect.x() + file as i32 * tile_size as i32;
+                let y = board_rect.y() + rank as i32 * tile_size as i32;
 
                 let square = Rect::new(
                     x,
